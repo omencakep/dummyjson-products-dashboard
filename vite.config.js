1d8c7494e73906315d536c74595e97e4e17c0d83
Raka Aditya <rakaaw3108@gmail.com>
2026-04-08 19:29:05 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-    plugins: [tailwindcss()],
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+
+            refresh: true,
+        }),
+    ],
 });
